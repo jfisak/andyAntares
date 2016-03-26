1784753aa99d5ce2582a529dbf64f83fd8bbc545
Jakub Fisak <fisak@physics.muni.cz>
2016-03-26 12:55:36 +0100

    partly rewritten for reading new version of compose_adata.dat

--- read_composition.f90
+++ read_composition.f90
@@ -12,87 +12,100 @@ SUBROUTINE read_composition()
   IMPLICIT NONE    
 
   INTEGER            :: I, J, flag
-  INTEGER            :: element_index, Z, lowerion, upperion
-  INTEGER            :: current_ion, nions,ios, NR
+  INTEGER            :: element_index, Z, lowerion, upperion, levels_type
+  INTEGER            :: current_element,current_ion, nions,ios, NR
   INTEGER, PARAMETER :: maxelements = 180
-  CHARACTER (20)     :: elementfile
+  CHARACTER (20)     :: filename
+  CHARACTER (100)     :: line
   CHARACTER (1)      :: junk
   DOUBLE PRECISION   :: mass
-  INTEGER, ALLOCATABLE :: elindexes(:)
 
-  OPEN (UNIT=7, FILE='compose_adata.dat')
-  ! computes number of lines in the input file
-  ! number of rows is equal to 0
-  ! for this time it will calculate number of rows
-  NR = 0
-  DO I=1,maxelements
-   READ(7,*,IOSTAT=ios) junk, junk, junk, junk, junk, junk
-    IF (ios /= 0) EXIT
-    IF (I == maxelements) THEN
-     print*, 'Error: Maximum number of records exceeded...'
-     print*, 'Exiting program now...'
-     STOP
+OPEN (UNIT=7, FILE='compose_adata.dat')
+ ! computes number of lines in the input file
+ ! number of rows is equal to 0
+ ! for this time it will calculate number of rows
+ n_elements = 0
+ DO I=1,maxelements
+  READ(7,*,IOSTAT=ios) line
+   IF (ios /= 0) EXIT
+   IF (I == maxelements) THEN
+    print*, 'Subroutine read_composition'
+    print*, 'Error: Maximum number of records exceeded...'
+    print*, 'Exiting program now...'
+    STOP
+   END IF
+   IF ( line == '**levels**' ) EXIT
+   IF ( INDEX(line, '*') /= 0) CYCLE
+  n_elements = n_elements + 1
+ END DO
+ ! Allocate the memory to the elements(n_elements)
+ print*, 'number of elements: ', n_elements
+ ALLOCATE (elements(n_elements)) 
+
+ REWIND(7)
+ ! Loop over all rows involved i.e. read all other lines in the compose_adata.dat
+ ! and assine these values to the elements(I)%... and elements(I)%ions(J)%...
+  I=1
+ DO 
+    READ(7,'(A)',iostat=ios) line
+   IF (ios /= 0) EXIT
+    IF ( TRIM(line) == '**levels**' ) THEN
+       print*, 'we have found the string **levels**...'
+       EXIT
     END IF
-   NR = NR + 1
-  END DO
-  REWIND(7)
-  print*, 'number of rows is equal to ', NR
-  ALLOCATE(elindexes(NR))
-  ! number of elements calculation
-  DO I = 1, NR
-     READ(7,*) element_index, Z, lowerion, upperion, mass, elementfile
-  END DO
- IF (NR .EQ. 1) THEN
-  n_elements = 1
- ELSE
-  n_elements=0
-  DO I = 2,NR
-   flag = 0
-   DO J = 1,I
-    IF(elindexes(J) .EQ. elindexes(I)) flag = 1
-   END DO
-    IF(flag .EQ. 0) n_elements = n_elements + 1
-  END DO
- END IF
-  ! Allocate the memory to the elements(n_elements)
-  print*, 'number of elements: ', n_elements
-  ALLOCATE (elements(n_elements)) 
+    IF ( INDEX(line, '*') /= 0) CYCLE
+    READ(line,*) element_index, Z, lowerion, upperion, mass
+    PRINT*, element_index, Z, lowerion, upperion, mass
+    elements(I)%atom_number = Z
+    elements(I)%atom_mass = mass * mp_g
+    ! Number of ions 
+    nions =  upperion - lowerion + 1
+    elements(I)%nions = nions
+    ! Assine lowerion to the current ion which we will use to caunt number of ions
+    ! This is important because we can play only with 3 and 4 ion.stage of some element
+    current_ion = lowerion
+    print*, 'current ion =', current_ion  
+    ! Allocate the memory to the elements(I)%ions(nions)
+    ALLOCATE (elements(I)%ions(nions))
+    ! Loop over all ions of given chem.element
+    DO J = lowerion, upperion
+       elements(I)%ions(J)%ion_stage = current_ion
+       current_ion = lowerion + 1
+    END DO
+    I=I+1
+ END DO
+ ! now reading atomic levels
+ DO 
+    READ(7,'(A)',iostat=ios) line
+  IF (ios /= 0) EXIT
+  IF ( TRIM(line) == '**transitions**' ) THEN
+      print*, 'we have found **transitions**...'
+      EXIT
+  END IF
+  IF ( INDEX(line, '*') /= 0) CYCLE
+  READ(line,*) current_element, lowerion, upperion, levels_type, filename
+  ! the most important is to read the filename
+  CALL read_atomic_data2(current_element,lowerion,upperion,levels_type,filename)
+ END DO
 
-  REWIND(7)
-  ! Loop over all rows involved i.e. read all other lines in the compose_adata.dat
-  ! and assine these values to the elements(I)%... and elements(I)%ions(J)%...
-  DO I = 1, NR
-     READ(7,*) element_index, Z, lowerion, upperion, mass, elementfile
-     PRINT*, element_index, Z, lowerion, upperion, mass
-     elements(I)%atom_number = Z
-     elements(I)%atom_mass = mass * mp_g
-     elements(I)%levelfile = elementfile
-     write(*,*) elementfile
-     ! Number of ions 
-     nions =  upperion - lowerion + 1
-     elements(I)%nions = nions
-     ! Assine lowerion to the current ion which we will use to caunt number of ions
-     ! This is important because we can play only with 3 and 4 ion.stage of some element
-     current_ion = lowerion
-     print*, 'current ion =', current_ion  
-     ! Allocate the memory to the elements(I)%ions(nions)
-     ALLOCATE (elements(I)%ions(nions))
-     ! Loop over all ions of given chem.element
-     DO J = lowerion, upperion
-        elements(I)%ions(J)%ion_stage = current_ion
-        current_ion = lowerion + 1
-     END DO
-  END DO
 
-  ! Only for testing
-  PRINT*, 'testing'
-  DO I = 1, n_elements
-     element_index = I
-     Z = elements(I)%atom_number
-     lowerion = elements(I)%ions(1)%ion_stage
-     upperion = elements(I)%ions(nions)%ion_stage
-     PRINT*, element_index, Z, lowerion, upperion
-  END DO
+
+
+
+
+
+
+
+
+ !!! Only for testing
+ PRINT*, 'testing'
+ DO I = 1, n_elements
+    element_index = I
+    Z = elements(I)%atom_number
+    lowerion = elements(I)%ions(1)%ion_stage
+    upperion = elements(I)%ions(nions)%ion_stage
+    PRINT*, element_index, Z, lowerion, upperion
+ END DO
 
 END SUBROUTINE read_composition
 
