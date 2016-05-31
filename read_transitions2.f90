d416b3e4a7f9a56e9f96b8051dc2f89012a2a033
Jakub Fisak <fisak@physics.muni.cz>
2016-05-31 09:40:46 +0200

    added files and comments

--- read_transitions2.f90
+++ read_transitions2.f90
@@ -105,74 +105,90 @@ OPEN (UNIT=9, status='old', FILE=filename)
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  CASE(2)
+  ! only if the variable linelis is allocated
   IF(ALLOCATED(linelist)) THEN
- DO 
-  DO
-    READ(9,'(A)',IOSTAT=ios) line
-    IF (ios /= 0) EXIT
-    IF(TRIM(line) == '**iont**') EXIT
-   END DO
-  DO
-    READ(9,'(A)',IOSTAT=ios) line
-    IF (ios /= 0) EXIT
-    IF ( INDEX(line, '*') /= 0) CYCLE
-    READ(line,*) current_element, current_ion, n_transitions
-    IF(n_transitions == 0) CYCLE
-    ! do we have the right file?
-    IF((current_element /= element) .OR. (current_ion < lowerion) &
-      .OR. (current_ion > upperion)) STOP 'WRONG ATOMIC TRANSITIONS...'
-   DO
-    READ(9,'(A)',IOSTAT=ios) line
-    IF (ios /= 0) STOP 'NO VALID DATA IN THE FILE...'
-    IF(TRIM(line) == '**lines**') EXIT
-   END DO
-   DO
-    READ(9,'(A)',IOSTAT=ios) line
-    IF (ios /= 0) EXIT
-    IF ( INDEX(line, '*') /= 0) CYCLE
-    READ(line,*) kindex, low_level, up_level, low_conf, up_conf, col_str, A, l_freq
-    n_line = ntransitions + 1
-    ntransitions = n_line
-    n_levels = elements(current_element)%ions(current_ion)%nlevels
-    DO I = 1,n_levels
-     IF(low_conf== elements(current_element)%ions(current_ion)%levels(I)%elconf) &
-        linelist(n_line)%lower = elements(current_element)%ions(current_ion)%levels(I)%l_index
-     IF(up_conf == elements(current_element)%ions(current_ion)%levels(I)%elconf) &
-        linelist(n_line)%upper = elements(current_element)%ions(current_ion)%levels(I)%l_index
-   END DO
-    linelist(n_line)%freq = 1.E-10*light_speed/l_freq
-    linelist(n_line)%f_ul = col_str
-    linelist(n_line)%A_ul = A
- END DO
-   IF(ios /= 0) EXIT
-   n_ions = n_ions + 1
-   IF(n_ions == (upperion - lowerion + 1)) EXIT
-  END DO   
-  END DO
+   ! do 0
+   DO 
+    ! do 1
+    ! looking for the flag **iont**
+    DO
+     READ(9,'(A)',IOSTAT=ios) line
+     IF (ios /= 0) EXIT
+     IF(TRIM(line) == '**iont**') EXIT
+    ! end do 1
+    END DO
+    ! do 5
+    DO
+      READ(9,'(A)',IOSTAT=ios) line
+      IF (ios /= 0) EXIT
+      IF ( INDEX(line, '*') /= 0) CYCLE
+      READ(line,*) current_element, current_ion, n_transitions
+      IF(n_transitions == 0) CYCLE
+      ! do we have the right file?
+      IF((current_element /= element) .OR. (current_ion < lowerion) &
+        .OR. (current_ion > upperion)) STOP 'WRONG ATOMIC TRANSITIONS...'
+     ! do 2
+     DO
+      READ(9,'(A)',IOSTAT=ios) line
+      IF (ios /= 0) STOP 'NO VALID DATA IN THE FILE...'
+      IF(TRIM(line) == '**lines**') EXIT
+     ! end do 2
+     END DO
+     ! do 3
+     DO
+      READ(9,'(A)',IOSTAT=ios) line
+      IF (ios /= 0) EXIT
+      IF ( INDEX(line, '*') /= 0) CYCLE
+      READ(line,*) kindex, low_level, up_level, low_conf, up_conf, col_str, A, l_freq
+      n_line = ntransitions + 1
+      ntransitions = n_line
+      n_levels = elements(current_element)%ions(current_ion)%nlevels
+      ! do 4
+      ! will find lower and upper index for every transition
+      DO I = 1,n_levels
+       IF(low_conf == elements(current_element)%ions(current_ion)%levels(I)%elconf) &
+          linelist(n_line)%lower = elements(current_element)%ions(current_ion)%levels(I)%l_index
+       IF(up_conf == elements(current_element)%ions(current_ion)%levels(I)%elconf) &
+          linelist(n_line)%upper = elements(current_element)%ions(current_ion)%levels(I)%l_index
+      ! end do 4
+      END DO
+      ! 
+      linelist(n_line)%freq = 1.E-10*light_speed/l_freq
+      linelist(n_line)%f_ul = col_str
+      linelist(n_line)%A_ul = A
+     ! end do 3
+     END DO
+     IF(ios /= 0) EXIT
+     n_ions = n_ions + 1
+     IF(n_ions == (upperion - lowerion + 1)) EXIT
+    ! end do 5
+    END DO   
+    ! end do 0
+    END DO
   ! if we do not have the ray linelist allocated, we have
   ! to calculate number of possible transitions
   ELSE
-  DO
-   READ(9,'(A)',IOSTAT=ios) line
-   IF (ios /= 0) STOP 'NO VALID DATA IN THE FILE...'
-   IF(TRIM(line) == '**iont**') EXIT
-  END DO
-  ! we will compute number of lines in the opened file
-  DO
-   READ(9,'(A)',IOSTAT=ios) line
-   print*, line
-   IF (ios /= 0) EXIT
-   IF ( INDEX(line, '*') /= 0) CYCLE
-   READ(line,*) junk, junk, n_transitions
-   ntransitions = ntransitions + n_transitions
-   IF(n_transitions == 0) CYCLE
-   ! loop which will find another label **iont**
-   DO 
-    READ(9,'(A)',IOSTAT=ios) line
-    IF (ios /= 0) EXIT
-    IF(TRIM(line) == '**iont**') EXIT
-   END DO
-  END DO
+    DO
+     READ(9,'(A)',IOSTAT=ios) line
+     IF (ios /= 0) STOP 'NO VALID DATA IN THE FILE...'
+     IF(TRIM(line) == '**iont**') EXIT
+    END DO
+    ! we will compute number of lines in the opened file
+    DO
+     READ(9,'(A)',IOSTAT=ios) line
+     print*, line
+     IF (ios /= 0) EXIT
+     IF ( INDEX(line, '*') /= 0) CYCLE
+     READ(line,*) junk, junk, n_transitions
+     ntransitions = ntransitions + n_transitions
+     IF(n_transitions == 0) CYCLE
+     ! loop which will find another label **iont**
+     DO 
+      READ(9,'(A)',IOSTAT=ios) line
+      IF (ios /= 0) EXIT
+      IF(TRIM(line) == '**iont**') EXIT
+     END DO
+    END DO
   END IF
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
