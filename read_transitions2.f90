2611b794885dc01d7402c898bf4bcb9820909a93
Jakub Fisak <fisak@pphysics.muni.cz>
2016-06-10 16:50:46 +0200

    debugged and work properly :-)

--- read_transitions2.f90
+++ read_transitions2.f90
@@ -71,8 +71,9 @@ CASE(0)
     linelist(n_line)%f_ul = oconstant * (elements(current_element)%ions(current_ion)%levels(up_level)%stat_waight / &
                        elements(current_element)%ions(current_ion)%levels(low_level)%stat_waight) *                 &
                        (linelist(n_line)%A_ul / linelist(n_line)%freq ** 2)
-    print*, n_line, ': ', 'indexe: ', linelist(n_line)%indexe, linelist(n_line)%indexi, linelist(n_line)%lower, &
-                       linelist(n_line)%upper, linelist(n_line)%freq, linelist(n_line)%A_ul, linelist(n_line)%f_ul
+    !print*, n_line, ': ', 'indexe: ', linelist(n_line)%indexe, linelist(n_line)%indexi, linelist(n_line)%lower, &
+    !                   linelist(n_line)%upper, 'f = ', linelist(n_line)%freq, linelist(n_line)%A_ul, linelist(n_line)%f_ul
+    print*, 'f = ', linelist(n_line)%freq
    END DO
    n_ions = n_ions + 1
    IF(n_ions == (upperion - lowerion + 1)) EXIT
@@ -173,9 +174,12 @@ CASE(0)
       ! 
       linelist(n_line)%indexe = current_element
       linelist(n_line)%indexi = current_ion
-      linelist(n_line)%freq = 1.E-10*light_speed/l_freq
+      linelist(n_line)%freq = 1.E+8*light_speed/l_freq
       linelist(n_line)%f_ul = col_str
       linelist(n_line)%A_ul = A
+!    print*, n_line, ': ', 'indexe: ', linelist(n_line)%indexe, linelist(n_line)%indexi, linelist(n_line)%lower, &
+!                       linelist(n_line)%upper, 'f = ', linelist(n_line)%freq, linelist(n_line)%A_ul, linelist(n_line)%f_ul
+    print*, 'f = ', linelist(n_line)%freq
       if(n_line == curr_n_tran) EXIT
      ! end do 3
      END DO
