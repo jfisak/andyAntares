2010c2b4a555819a569fcdae792347df3b3f3896
Jakub Fisak <fisak@pphysics.muni.cz>
2016-06-11 09:43:32 +0200

    debugging

--- event_dist.f90
+++ event_dist.f90
@@ -110,6 +110,8 @@ SUBROUTINE event_dist(pack_index, cell_dist, e_dist, event)
         g_ll = elements(indexe)%ions(indexi)%levels(lower_level)%stat_waight
         e_exc = elements(indexe)%ions(indexi)%levels(lower_level)%exci_energy - elements(indexe)%ions(indexi)%levels(1)%exci_energy
         pop_number = graund_level_pop * g_ll / g_gl * exp(e_exc / BOLK / model_grid(current_mgi)%T )
+        print*, 'e_exc1 = ', elements(indexe)%ions(indexi)%levels(lower_level)%exci_energy, 'e_exc2 = ', &
+                elements(indexe)%ions(indexi)%levels(1)%exci_energy
         !Needs proper treatment of empty cells
         !IF (vec_length(package(dummypackage)%pos) .GT. R_inf) pop_number = 0
         tau_line = light_speed / freq_line * constant * linelist(next_line)%f_ul * pop_number * &
