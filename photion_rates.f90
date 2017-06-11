00150f7907979639c1644afe6518f297a749df3d
Jakub Fisak <fisak@physics.muni.cz>
2017-06-11 08:23:15 +0200

    Calculation of photoionization cross section
    * Zphot is now calculated correctly (now only from the Planck function)
    * found a mistake in read_photcs -- frequency was twice multiplied by
      the f_0 and it was much larger than it should be
    * sbr radiation_field is not used now

--- photion_rates.f90
+++ photion_rates.f90
@@ -1,8 +1,47 @@
-SUBROUTINE photion_rates()
+SUBROUTINE photion_rates(approx, indexe, indexi, leveli, current_mgi, act_pop, phot_cross)
 USE types
 IMPLICIT NONE
 
+! input variables
+INTEGER                                 :: approx, indexe, indexi, leveli
+INTEGER                                 :: current_mgi
+DOUBLE PRECISION                        :: act_pop
+! computing fields
+INTEGER                                 :: npoints
+INTEGER                                 :: I
+DOUBLE PRECISION, ALLOCATABLE           :: freq(:), cross(:), func(:)
+DOUBLE PRECISION                        :: planck
+DOUBLE PRECISION                        :: summ
+! output variables
+DOUBLE PRECISION                        :: phot_cross
 
-
+SELECT CASE(approx)
+! the most stupid approximation: J is the Planck function
+CASE(0)
+ npoints = SIZE(elements(indexe)%ions(indexi)%levels(leveli)%photcros(1,:))
+ print*, 'photion_rates: npoints = ', npoints
+ IF(npoints == 0) THEN
+  phot_cross = 0.D0
+  RETURN
+ END IF
+ ALLOCATE(freq(npoints), cross(npoints), func(npoints))
+ T_eff = model_grid(current_mgi)%T
+ freq(1:npoints) = elements(indexe)%ions(indexi)%levels(leveli)%photcros(1,1:npoints)
+ cross(1:npoints) = elements(indexe)%ions(indexi)%levels(leveli)%photcros(2,1:npoints)
+ summ = 0.D0
+ DO I = 1, npoints
+  planck = ( 2.D0 * h * freq(I)**3 / light_speed**2 )  * &
+          1.D0 / ( EXP( (h * freq(I) / (BOLK * T_eff) ) - 1.D0 ) )
+  func(I) = cross(I) * planck / ( h * freq(I))
+ END DO
+ DO I = 1, npoints - 1
+  summ = summ + (func(I) + func(I + 1)) / 2.D0 * (freq(I + 1) - freq(I))
+ END DO
+ print*, 'phot_cross = ', phot_cross
+ phot_cross = 4 * pi * summ * act_pop
+ print*, 'photion_rates: phot_cross = ', phot_cross
+CASE DEFAULT
+ STOP 'photion_rates: this approximation is not known'
+END SELECT
 
 END SUBROUTINE photion_rates
--- read_photcs.f90
+++ read_photcs.f90
@@ -69,7 +69,7 @@ CASE(2)
     ALLOCATE(elements(indexe)%ions(indexI)%levels(indexclev)%photcros(2,nofPoints))
    !print*, 'nofPoints = ', nofPoints
    ! we can compute a frequency treshold from these data
-   freqt = abs(energy * Rydberg) / h
+   freqt = abs(energy * Rydberg * e_v) / h
    ! now we will read the given data for the photoionization cross section
    DO I = 1, nofPoints
     READ(13, '(A)', IOSTAT=ios) line
@@ -80,7 +80,7 @@ CASE(2)
     !print*, freq(I), cross(I)
     IF(save_cs .EQV. .TRUE.) THEN
      freq = freq * freqt
-     elements(indexe)%ions(indexI)%levels(indexclev)%photcros(1,I) = freq * freqt
+     elements(indexe)%ions(indexI)%levels(indexclev)%photcros(1,I) = freq
      elements(indexe)%ions(indexI)%levels(indexclev)%photcros(2,I) = cross
      ! print*, index, indexI, freq(I), cross(I)
     END IF
