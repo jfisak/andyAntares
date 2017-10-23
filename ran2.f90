324295a40d971b24f96e98f5118a558910181bb4
Jakub Fisak <fisak@physics.muni.cz>
2017-10-23 16:16:11 +0200

    trying to include a new class but still have not solved problems during compilation

--- ran2.f90
+++ ran2.f90
@@ -1,23 +1,24 @@
-DOUBLE PRECISION   FUNCTION  ran2(idum)
+SUBROUTINE  ran2(idum)
 
-USE ran2_class
-
+ USE ran2_class
  IMPLICIT NONE 
- CLASS(ranvar), POINTER      :: this
 
- INTEGER PARAMETER           :: IM1 = 2147483563, IM2 = 2147483399, IMM1 = IM1 - 1, &
+
+ INTEGER, PARAMETER          :: IM1 = 2147483563, IM2 = 2147483399, IMM1 = IM1 - 1, &
                                 IA1=40014,IA2=40692,IQ1=53668,IQ2=52774,IR1=12211,IR2=3791, &
                                 NDIV=1+IMM1/NTAB
  INTEGER                     :: j,k
  INTEGER                     :: NTAB, iy, idum2
  INTEGER, DIMENSION(NTAB)    :: iv
+ CLASS(ranvar)      :: ranum
+ DOUBLE PRECISION            :: out_ran
 
- IF(.NOT. ASSOCIATED(this)) this = initialize_values()
+ IF(.NOT. ASSOCIATED(ranum)) ranum%init()
 
- NTAB = this%NTAB
- iy = this%iy
- idum2 = this%idum2
- iv(:) = this%iv(:)
+ NTAB = ranum%NTAB
+ iy = ranum%iy
+ idum2 = ranum%idum2
+ iv(:) = ranum%iv(:)
 
  if (idum.le.0) then
   idum=max(-idum,1)
@@ -40,9 +41,9 @@ USE ran2_class
  iy=iv(j)-idum2
  iv(j)=idum
  if(iy.lt.1)iy=iy+IMM1
- ran2=min(AM*iy,RNMX)
+ out_ran=min(AM*iy,RNMX)
 
- RETURN
+ RETURN out_ran
 
-END FUNCTION 
+END SUBROUTINE 
 
--- ran2_class.f90
+++ ran2_class.f90
@@ -5,27 +5,34 @@ MODULE ran2_class
 
  TYPE, PRIVATE :: ranvar
  
-  INTEGER, PARAMETER              :: NTAB = 32
-  INTEGER, DIMENSION(NTAB)        :: iv
+  INTEGER                         :: NTAB
+  INTEGER, ALLOCATABLE            :: iv(:)
   INTEGER                         :: iy
   INTEGER                         :: idum2
   
   CONTAINS
 
-  PROCEDURE, PUBLIC :: initialize_values
+  PROCEDURE, PUBLIC :: init => initialize_values
  END TYPE ranvar
 
+ PRIVATE                          :: initialize_values
 
- SUBROUTINE initialize_values()
+ CONTAINS
+
+ SUBROUTINE initialize_values(this)
   CLASS(ranvar)         :: this
   ! loop index
   INTEGER               :: ind
+  INTEGER               :: NTAB
 
+  NTAB = 32
+  this%NTAB = NTAB
+  IF(.NOT. ALLOCATED(this%iv)) ALLOCATE(this%iv(NTAB))
   DO ind = 1, NTAB
    this%iv(ind) = 0
   END DO
   this%iy = 0
   this%idum2 = 123456789
- END SUBROUTINE
+ END SUBROUTINE initialize_values
 
 END MODULE ran2_class
