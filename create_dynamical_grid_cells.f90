041eca210d471db07e4dd7764b7aad74692b3f3a
Jakub Fisak <fisak@physics.muni.cz>
2021-03-04 10:28:39 +0100

    repaired a bug during the ijk grid creation

--- create_dynamical_grid_cells.f90
+++ create_dynamical_grid_cells.f90
@@ -224,23 +224,9 @@ END DO
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 CASE(2)
  IF(model_type == 1) THEN
-  current_mgi = dyn_cell(n_dyncell)%model_index
-  IF (current_mgi /= n_modelgrid + 1) THEN
-   width = model_grid(current_mgi)%width
-   ! dimofsubcells(1) = CEILING(cell_width_2(1) / width)
-   ! dimofsubcells(2) = CEILING(cell_width_2(2) / width)
-   ! dimofsubcells(3) = CEILING(cell_width_2(3) / width)
-   dimofsubcells(1) = FLOOR(np**(1.0/2.0))
-   dimofsubcells(2) = FLOOR(np**(1.0/2.0))
-   dimofsubcells(3) = FLOOR(np**(1.0/2.0))
-   ! write(*,*) 'create_dynamical_grid_cells: current_mgi = ', current_mgi
-   ! write(*,*) 'create_dynamical_grid_cells: cell_width_2 = ', cell_width_2(:), ' width = ', width
-   ! write(*,*) 'create_dynamical_grid_cells: dimofsubcells = ', dimofsubcells(:)
-  ELSE
-   dimofsubcells(1) = 1
-   dimofsubcells(2) = 1
-   dimofsubcells(3) = 1
-  END IF
+  dimofsubcells(1) = FLOOR(np**(1.0/2.0))
+  dimofsubcells(2) = FLOOR(np**(1.0/2.0))
+  dimofsubcells(3) = FLOOR(np**(1.0/2.0))
  ELSE
   dimofsubcells(1) = FLOOR(np**(4.0/1.0))
   dimofsubcells(2) = FLOOR(np**(4.0/1.0))
