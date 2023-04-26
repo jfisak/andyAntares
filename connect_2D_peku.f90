721f4f3888274338294750dfe4bc1156a1f54711
Jakub Fisak <fisak@physics.muni.cz>
2023-04-26 15:06:42 +0200

    efektivni connect 2D PeKu

--- connect_2D_peku.f90
+++ connect_2D_peku.f90
@@ -12,10 +12,17 @@ INTEGER                                 :: cur_ind_A, cur_ind_B, cur_vpg_cell
 DOUBLE PRECISION                        :: cur_r, cur_t
 DOUBLE PRECISION                        :: rmax, rmin, tmax, tmin
 
+INTEGER, DIMENSION (n_modelgrid,2)        :: vg_indexy_A, vg_indexy_B
+INTEGER, DIMENSION(2)                   :: dummy_var, dummy
 INTEGER, ALLOCATABLE                    :: n_points_A(:), n_points_B(:)
 INTEGER, ALLOCATABLE                    :: dummy_counter_A(:), dummy_counter_B(:)
 INTEGER, ALLOCATABLE                    :: indices_A(:), indices_B(:)
 
+INTEGER                                 :: I
+
+!!!!!!!!!!!!!!!!!!!!!!!
+! ZATÍM JEN VG A JE IMPLEMENTOVÁN
+!!!!!!!!!!!!!!!!!!!!!!!
 
 ! virtual grid definition
 ! division of virGrid A and B
@@ -35,23 +42,50 @@ tmin = MINVAL(model_grid(:)%angle)-1e-2
 w_vgrid_r = abs(rmax - rmin)/N_vgrid_r
 w_vgrid_t = abs(tmax - tmin)/N_vgrid_t
 
-
-
 ! calculation of the virGrid index
 DO cur_point = 1, n_modelgrid
  cur_r = model_grid(cur_point)%rwind
  cur_t = model_grid(cur_point)%angle
  cur_n_r_A = floor(cur_r/w_vgrid_r) + 1
  cur_n_t_A = floor(cur_t/w_vgrid_t) + 1
- cur_n_r_B = floor(cur_r/w_vgrid_r + 1/2) + 1
- cur_n_t_B = floor(cur_t/w_vgrid_t + 1/2) + 1
+ cur_n_r_B = floor(cur_r/w_vgrid_r + 1.0/2.0) + 1
+ cur_n_t_B = floor(cur_t/w_vgrid_t + 1.0/2.0) + 1
 
+ ! n_rt_A -- numerical index of VG cell
  n_rt_A = cur_n_r_A + N_vgrid_r * (cur_n_t_A - 1)
  ! write(*,*) 'connect_2D_peku: cur_n_t_A = ', cur_n_t_A, ' cur_n_r_A = ', cur_n_r_A, ' n_rt_A = ', n_rt_A
+ ! n_points -- number of points for the given cell
  n_points_A(n_rt_A) = n_points_A(n_rt_A) + 1
- n_rt_B = cur_n_r_B + N_vgrid_r * (cur_n_t_B - 1)
- n_points_B(n_rt_B) = n_points_B(n_rt_B) + 1
+ ! vg_indexy -- list of indeces model grid --> VG index point
+ vg_indexy_A(cur_point, 1) = n_rt_A
+ vg_indexy_A(cur_point, 2) = cur_point
+ !!!!!!!!
+ ! repete
+ ! n_rt_B = cur_n_r_B + N_vgrid_r * (cur_n_t_B - 1)
+ ! n_points_B(n_rt_B) = n_points_B(n_rt_B) + 1
+ ! vg_indexy_B(cur_point, 1) = n_rt_B
+ ! vg_indexy_B(cur_point, 2) = cur_point
 END DO
+
+write(*,*) 'connect_2D_peku: vg_indexy_A = ', vg_indexy_A(:,2)
+write(*,*) '**************************************************'
+
+! sort the vg_indexy according to the VG index
+DO cur_point = 2, n_modelgrid
+ I = cur_point - 1
+
+ dummy_var = vg_indexy_A(cur_point,:)
+
+ DO WHILE(I > 1)
+  IF(vg_indexy_A(I,1) > dummy_var(1)) THEN
+   dummy = vg_indexy_A(I + 1,:)
+   vg_indexy_A(I + 1,:) = vg_indexy_A(I,:)
+   vg_indexy_A(I,:) = dummy
+  END IF
+  I = I - 1
+ END DO
+END DO
+write(*,*) 'connect_2D_peku: vg_indexy_A = ', vg_indexy_A(:,2)
 
 ! index array
 cur_ind_A = 0
@@ -60,17 +94,19 @@ cur_ind_B = 0
 ! create arrays with indeces pointing to an ordered list of modCell grids indeces
 DO cur_vpg_cell = 1, N_vgrid_cells
  indices_A(cur_vpg_cell) = cur_ind_A + 1
- indices_B(cur_vpg_cell) = cur_ind_B + 1
+ !indices_B(cur_vpg_cell) = cur_ind_B + 1
 
  cur_ind_A = cur_ind_A + n_points_A(cur_vpg_cell)
- cur_ind_B = cur_ind_B + n_points_B(cur_vpg_cell)
+ !cur_ind_B = cur_ind_B + n_points_B(cur_vpg_cell)
 
 END DO
+write(*,*) 'connect_2D_peku: indices_A = ', indices_A
 
 ! 
 dummy_counter_A = n_points_A
 dummy_counter_B = n_points_B
 
+! 
 
 
 
