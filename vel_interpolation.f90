f18c0bcb8305705b9279464354c2db27022014ec
Jakub Fisak <fisak@physics.muni.cz>
2024-04-02 13:08:02 +0200

    velocity field interpolation
    Now the problem with finding the closest points have been solved. An
    array is calculated correctly.
    
    * added comments for a better orientation in the sbr
    * the sbr is capable to set up an array with n closest points

--- vel_interpolation.f90
+++ vel_interpolation.f90
@@ -28,7 +28,7 @@ INTEGER                                 :: cur_ind_A, cur_ind_B, cur_vpg_cell
 
 INTEGER                                 :: n_zeros
 
-DOUBLE PRECISION, DIMENSION(3)          :: cur_pos
+DOUBLE PRECISION, DIMENSION(3)          :: cur_pg_pos, cur_mg_pos
 INTEGER                                 :: cur_prop_cell
 
 DOUBLE PRECISION, PARAMETER             :: large_number = 1.D90
@@ -39,14 +39,19 @@ INTEGER                                 :: cur_start_index, cur_end_index, cur_n
 DOUBLE PRECISION                        :: dist_A, dist_B
 
 INTEGER, PARAMETER                      :: n_closest = 6
-DOUBLE PRECISION, ALLOCATABLE           :: interp_vel(:,:), interp_pos(:,:), interp_dist(:)
+DOUBLE PRECISION, ALLOCATABLE           :: interp_vel(:,:), interp_pos(:,:), interp_dist(:,:), pom(:,:)
 
 INTEGER                                 :: chosen_gridAB
 INTEGER, PARAMETER                      :: grid_A = 1, grid_B = 2
+INTEGER, PARAMETER                      :: ind_dist = 1, ind_index = 2
 
 DOUBLE PRECISION, DIMENSION(3)          :: cur_centre
 INTEGER                                 :: cur_nearest_point, cur_nop, cur_vg_index, cur_vg_point
 INTEGER                                 :: cur_nx, cur_ny, cur_nz
+INTEGER                                 :: cip, cur_index, cur_index_pos
+DOUBLE PRECISION                        :: dist
+LOGICAL                                 :: seeking
+INTEGER                                 :: cur_index_i
 
 !_______________________________________________________________
 !   #00             SET UP OF VIRTUAL GRIDS
@@ -162,6 +167,7 @@ DO cur_vpg_cell = 1, N_vgrid_cells_B
  cur_ind_B = cur_ind_B + n_points_B(cur_vpg_cell)
 END DO
 
+ALLOCATE(interp_dist(n_closest,ind_index), pom(n_closest, ind_index))
 ! write(*,*) 'vel_interpolation: indices_B = ', indices_B
 !_______________________________________________________________
 !  #03              INTERPOLATING
@@ -169,8 +175,11 @@ END DO
 
 ! going through the propGrid and finding a velocity vector using the trilinear interpolation
 DO cur_prop_cell = 1, n_propgcells
+ !_______________________________________________________________
+ !  #              CHOISE OF GRID A OR B
+ !_______________________________________________________________
  ! the index in the AB grid
- cur_pos = dyn_cell(cur_prop_cell)%corner + dyn_cell(cur_prop_cell)%width/2.D0
+ cur_pg_pos = dyn_cell(cur_prop_cell)%corner + dyn_cell(cur_prop_cell)%width/2.D0
  cur_n_x_A = floor((cur_x - xmin)/w_vgrid_x) + 1
  cur_n_y_A = floor((cur_y - ymin)/w_vgrid_y) + 1
  cur_n_z_A = floor((cur_z - zmin)/w_vgrid_z) + 1
@@ -190,16 +199,19 @@ DO cur_prop_cell = 1, n_propgcells
  END IF
 
  ! what is the best grid, A or B?
- dist_A = sqrt((cur_pos(1) - cur_center_A(1))**2 + (cur_pos(2) - cur_center_A(2))**2 + &
-   & (cur_pos(3) - cur_center_A(3))**2)
+ dist_A = sqrt((cur_pg_pos(1) - cur_center_A(1))**2 + (cur_pg_pos(2) - cur_center_A(2))**2 + &
+   & (cur_pg_pos(3) - cur_center_A(3))**2)
  IF(n_b > 0) THEN
-  dist_B = sqrt((cur_pos(1) - cur_center_B(1))**2 + (cur_pos(2) - cur_center_B(2))**2 + &
-   &(cur_pos(3) - cur_center_B(3))**2)
+  dist_B = sqrt((cur_pg_pos(1) - cur_center_B(1))**2 + (cur_pg_pos(2) - cur_center_B(2))**2 + &
+   &(cur_pg_pos(3) - cur_center_B(3))**2)
  ELSE IF (n_B == 0) THEN
   dist_B = large_number
  END IF
   
 
+ !_______________________________________________________________
+ !               CHOISE OF MODGRID POINTS
+ !_______________________________________________________________
  ! choosing the correct modGrid points
  ! this is done for unification of the forthcoming code (after this if)
  IF(dist_A < dist_B) THEN
@@ -217,6 +229,10 @@ DO cur_prop_cell = 1, n_propgcells
   cur_start_index = indices_B(n_B)
   cur_end_index = cur_start_index + cur_n_points - 1
 
+  write(*,*) 'vel_interpolation: n_closest = ', n_closest
+  ! interp_dist(I,J)
+  ! interp_dist(:,1) -- distance
+  ! interp_dist(:,2) -- index of the point
   ALLOCATE(cur_points(cur_n_points))
 
   cur_points = vg_indexy_B(cur_start_index:cur_end_index,2)
@@ -226,37 +242,61 @@ DO cur_prop_cell = 1, n_propgcells
   chosen_gridAB = grid_B
  END IF ! dist_A < dist_B
 
+ ! DO cur_index_i = 1, SIZE(cur_points)
+ !  write(*,*) 'vel_interpolation: pos = ', model_grid(cur_index_i)%vec_pos
+ ! END DO
  write(*,*) 'vel_interpolation: cur_start_index = ', cur_start_index, ' cur_end_index = ', cur_end_index
  write(*,*) 'vel_interpolation: cur_n_points = ', cur_n_points
 
  
- ALLOCATE(interp_vel(cur_n_points, 3), interp_pos(cur_n_points, 3), interp_dist(cur_n_points))
-
- interp_dist(:) = large_number
- 
- ! seeking the n_closest closest points
+ !_______________________________________________________________
+ !            SEEKING FOR THE n_closest CLOSEST POINTS
+ !_______________________________________________________________
+ interp_dist(:,ind_dist) = large_number
+ seeking = .TRUE.
+ write(*,*) 'vel_interpolation: interp_dist = ', interp_dist(:,ind_dist)
  DO cur_nearest_point = 1, cur_n_points
   cur_nop = cur_points(cur_nearest_point)
-  IF(chosen_gridAB == grid_A) THEN
-   cur_nx = floor((cur_x-xmin)/w_vgrid_x) + 1
-   cur_ny = floor((cur_y-ymin)/w_vgrid_y) + 1
-   cur_nz = floor((cur_z-zmin)/w_vgrid_z) + 1
-   cur_centre = (/ xmin + (cur_nx + 5D-1) * w_vgrid_x, ymin + (cur_ny + 5D-1) * w_vgrid_y, &
-    & zmin + (cur_nz + 5D-1) * w_vgrid_z/)
-  ELSE IF(chosen_gridAB == grid_B) THEN
-   cur_nx = floor((cur_x - xmin)/w_vgrid_x - 1.0/2.0) + 1
-   cur_ny = floor((cur_y - ymin)/w_vgrid_y - 1.0/2.0) + 1
-   cur_nz = floor((cur_z - zmin)/w_vgrid_z - 1.0/2.0) + 1
-   cur_centre = (/ xmin + (cur_nx + 1.D0) * w_vgrid_x, ymin + (cur_ny + 1.D0) * w_vgrid_y, &
-    & zmin + (cur_nz + 1.D0) * w_vgrid_z /)
-  END IF
- END DO
+  cur_mg_pos = model_grid(cur_nop)%vec_pos
+  write(*,*) 'vel_interpolation: cur_index = ', cur_nearest_point, ' cur_mg_pos = ', cur_mg_pos
+  
+  ! distance of a current modGrid point and the 
+  dist = sqrt((cur_pg_pos(1) - cur_mg_pos(1))**2 + (cur_pg_pos(2) - cur_mg_pos(2))**2 + &
+   & (cur_pg_pos(3) - cur_mg_pos(3))**2)
 
+  ! going through the array interp_dist and sorting from the smallest distances to largest distances
+  DO cur_index_pos = 1, n_closest
+   ! the current distance in the array is larger than the current distance, we should move all values
+   ! one level below and save the new distance to the current index
+   !write(*,*) 'vel_interpolation: intpdist = ', interp_dist(cur_index_pos, ind_dist), ' dist = ', dist
+   seeking = .TRUE.
+   IF(interp_dist(cur_index_pos, ind_dist) > dist) THEN
+    pom = interp_dist
+    DO cip = cur_index_pos, n_closest
+     write(*,*) 'vel_interpolation: cip = ', cip, ' cur_index_pos = ', cur_index_pos
+     IF(cip == cur_index_pos) THEN
+      interp_dist(cip, ind_dist) = dist
+      interp_dist(cip, ind_index) = cur_nop
+     ELSE
+      interp_dist(cip, ind_dist) = pom(cip - 1, ind_dist)
+      interp_dist(cip, ind_index) = pom(cip - 1, ind_index)
+     END IF
+     seeking = .FALSE.
+    END DO
+   END IF ! cur_dist < dist in array
+   IF(.NOT. seeking) EXIT
+  END DO ! cur_index_pos
+  ! write(*,*) 'vel_interpolation: interp_dist = ', interp_dist(:,ind_dist)
+ END DO
+ 
+ write(*,*) 'vel_interpolation:************************************************'
+ write(*,*) 'vel_interpolation: interp_dist = ', interp_dist(:,ind_dist)/R_star
+ STOP 'vel_interpolation: testing'
  DEALLOCATE(cur_points, interp_vel, interp_pos)
 
 END DO
 
- STOP 'vel_interpolation: testing'
+STOP 'vel_interpolation: testing'
 
 
 
