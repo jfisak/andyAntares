b70e7add2d34331dc5f2ff31b09a0c4623af0c63
Jakub Fisak <fisak@physics.muni.cz>
2025-04-10 11:47:13 +0200

    small repair

--- roverw.f90
+++ roverw.f90
@@ -107,7 +107,7 @@ ELSE IF(velApprox == 5) THEN
  R_pos = norm2(package(pack_index)%pos)
  R_pos_vec = package(pack_index)%pos / norm2(package(pack_index)%pos)
  costheta = dot_product(package(pack_index)%dir, R_pos_vec)
- part_1 = costheta**2*8.0 R_pos/(R_inf - R_star) * cos(8*R_pos/(R_inf - R_star))
+ part_1 = costheta**2*8.0* R_pos/(R_inf - R_star) * cos(8*R_pos/(R_inf - R_star))
  part_2 = (1-costheta**2) * sin(8*R_pos/(R_inf - R_star))
  part_3 = 1/(2 * param_k)
  IF(R_pos <= R_star .or. R_pos > R_inf) THEN
