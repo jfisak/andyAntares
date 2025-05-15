048eb7a47269718e6b9fc28f697c66ef3ef7c63f
Jakub Fisak <fisak@physics.muni.cz>
2025-05-15 09:07:58 +0200

    cleaning of the code

--- save_propmod_grid.f90
+++ save_propmod_grid.f90
@@ -1,17 +1,21 @@
 ! this sbr saves propagation and model grid into a single file for a purpose to read
 ! it later instead of creating it in every run
+!
+! INPUT: NONE
+! OUTPUT: NONE
+!
 SUBROUTINE save_propmod_grid()
 
 USE types
 USE constants
 IMPLICIT NONE
 
-INTEGER                                         :: I
-CHARACTER(LEN=60)                               :: propmod_file
+INTEGER                                         :: ind_I
+CHARACTER(LEN=filename_length)                  :: propmod_file
 
 INTEGER                                         :: n_propgrid
 
-DOUBLE PRECISION, DIMENSION(3)                  :: pos, vel, width
+DOUBLE PRECISION, DIMENSION(const_dimofspace)                  :: pos, vel, width
 DOUBLE PRECISION                                :: rho, temp, volume, el_dens
 
 INTEGER                                         :: down_cell, up_cell, model_index, assoc_cells
@@ -36,31 +40,31 @@ write(49,*) xmax, ymax, zmax
 write(49,*) nx_cell, ny_cell, nz_cell
 write(49,*) add_mg
 
-DO I = 1, n_modelgrid
+DO ind_I = 1, n_modelgrid
  IF(model_type == 1) THEN
-  pos(1) = model_grid(I)%rwind
+  pos(1) = model_grid(ind_I)%rwind
   pos(2) = 0.e0
   pos(3) = 0.e0
-  vel(1) = model_grid(I)%vel
+  vel(1) = model_grid(ind_I)%vel
   vel(2) = 0.e0
   vel(3) = 0.e0
  ELSE IF(model_type == 2) THEN
-  pos(1) = model_grid(I)%rwind
-  pos(2) = model_grid(I)%angle
+  pos(1) = model_grid(ind_I)%rwind
+  pos(2) = model_grid(ind_I)%angle
   pos(3) = 0.e0
-  vel(1) = model_grid(I)%vel
-  vel(2) = model_grid(I)%velang
+  vel(1) = model_grid(ind_I)%vel
+  vel(2) = model_grid(ind_I)%velang
   vel(3) = 0.e0
  ELSE IF(model_type == 3) THEN
-  pos = model_grid(I)%vec_pos
-  vel = model_grid(I)%vec_vel
+  pos = model_grid(ind_I)%vec_pos
+  vel = model_grid(ind_I)%vec_vel
  END IF
- rho = model_grid(I)%rho
- temp = model_grid(I)%T
- volume = model_grid(I)%volume
- assoc_cells = model_grid(I)%assoc_cells
- el_dens = model_grid(I)%e_dens
- is_diff = model_grid(I)%is_difapp
+ rho = model_grid(ind_I)%rho
+ temp = model_grid(ind_I)%T
+ volume = model_grid(ind_I)%volume
+ assoc_cells = model_grid(ind_I)%assoc_cells
+ el_dens = model_grid(ind_I)%e_dens
+ is_diff = model_grid(ind_I)%is_difapp
 
  write(49,*) pos, vel, rho, temp, volume, assoc_cells, el_dens, is_diff
 END DO
@@ -68,13 +72,13 @@ END DO
 ! saves the associated propagation grid
 n_propgrid = SIZE(dyn_cell)
 write(49,*) n_propgrid
-DO I = 1, n_propgrid
- pos = dyn_cell(I)%corner
- width = dyn_cell(I)%width
- up_cell = dyn_cell(I)%up_cell
- down_cell = dyn_cell(I)%down_cell
- neighbors = dyn_cell(I)%neighbor
- model_index = dyn_cell(I)%model_index
+DO ind_I = 1, n_propgrid
+ pos = dyn_cell(ind_I)%corner
+ width = dyn_cell(ind_I)%width
+ up_cell = dyn_cell(ind_I)%up_cell
+ down_cell = dyn_cell(ind_I)%down_cell
+ neighbors = dyn_cell(ind_I)%neighbor
+ model_index = dyn_cell(ind_I)%model_index
 
  write(49,*) pos, width, up_cell, down_cell, neighbors, model_index
 END DO
