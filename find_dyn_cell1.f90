7068b9e9a2c05e11c0a3749b9e023479e03804ed
Jakub Fisak <fisak@physics.muni.cz>
2025-06-18 11:17:06 +0200

    corrected find_dyn_cell1

--- find_dyn_cell1.f90
+++ find_dyn_cell1.f90
@@ -151,6 +151,8 @@ ELSE
  sign_z = .false.
 END IF
 
+cur_cell = actCell
+
 DO
  IF(dyn_cell(cur_cell)%up_cell == 0) THEN
   obtained_cell = cur_cell
@@ -205,6 +207,20 @@ DO
  ELSE
   write(*,*) 'find_dyn_cell1: xyz = ', sign_x, sign_y, sign_z
   STOP 'find_dyn_cell1: error in signs, this combination is not implemented'
+ END IF
+ IF(debug == 2) THEN
+  corner = dyn_cell(cur_cell)%corner
+  width = dyn_cell(cur_cell)%width
+  DO ind_I = 1, const_dimofspace
+   IF(((pos(ind_I) < corner(ind_I) - mininum) .OR. (pos(ind_I) > corner(ind_I) + width(ind_I) + mininum))) THEN
+    write(*,*) 'find_dyn_cell1: cell starting = ', corner
+    write(*,*) 'find_dyn_cell1: packet pos = ', pos
+    write(*,*) 'find_dyn_cell1: cell ending = ', (corner + width)
+    write(*,*) 'find_dyn_cell1: ind_I = ', ind_I
+    write(*,*)  'find_dyn_cell1: packet is not located inside the propagation cell'
+    STOP
+   END IF
+  END DO
  END IF
  ! we have found a cell containing the given point
  ! is this cell on the top of the dyncell tree?
